6e79b322cab811be0f04802ff77b95e16e1d100f
zd21 <zdhussain21@gmail.com>
2020-06-10 21:08:43 +0530

    now again id di it

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("yes we got a new laptop today");
+        display();
+    }
+
+
+    public static void display() {
+        System.out.println("yeah we successfully called a new function");
+    }
+}
