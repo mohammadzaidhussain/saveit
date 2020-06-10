bee8365e97c9e2b421bd9c2fcec3390697092853
zd21 <zdhussain21@gmail.com>
2020-06-10 21:11:32 +0530

    last time i did it

--- Demo.java
+++ Demo.java
@@ -7,5 +7,6 @@ public class Demo {
 
     public static void display() {
         System.out.println("yeah we successfully called a new function");
+        System.out.println("one line added");
     }
 }
